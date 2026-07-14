546119dee735f9cebde550171b27099ff50e7147
Steven Victor Mycynek <smycynek@users.noreply.github.com>
2026-07-14 19:44:49 -0400

    New spline lib, add curvature (#2)

--- src/nurbs.ts
+++ src/nurbs.ts
@@ -35,6 +35,19 @@ export function createSplineNurbTangents(points: Point[], degree: number) {
   return interPoints;
 }
 
+function calculateCurvature(curve: ReturnType<typeof nurbs>, t: number): number {
+  const d1val: [number, number] = curve.evaluator(1)([], t);
+  const d2val: [number, number] = curve.evaluator(2)([], t);
+  const firstDerivative = new Vector(...d1val, 0); // make it a 3D vector for cross product
+  const secondDerivative = new Vector(...d2val, 0);
+  const speed = firstDerivative.magnitude(); // speed is length of first derivative
+  if (speed === 0) {
+    return 0;
+  }
+  // first cross second / (length of first cubed)
+  return firstDerivative.cross(secondDerivative).magnitude() / (speed * speed * speed);
+}
+
 export function createSplineNurbNormals(points: Point[], degree: number) {
   const sPoints = points.map((p) => [p.x, p.y]);
   const curve = nurbs(sPoints, degree);
@@ -45,10 +58,12 @@ export function createSplineNurbNormals(points: Point[], degree: number) {
     const p1a = curve.evaluate([], idx);
     const p1 = new Point(p1a[0], p1a[1]);
     const dval = derivative([], idx);
-    const tangent = new Vector(dval[0], dval[1], 0);
-    const normal = tangent.cross(new Vector(0, 0, -1)).normalize();
+    const tangent = new Vector(dval[0], dval[1], 0); // make it a 3D vector for cross product
+    const normal = tangent.cross(new Vector(0, 0, -1)).normalize(); // noraml is perpendicular to tangent;
 
-    const p2 = new Point(p1.x + normal.x, p1.y + normal.y);
+    const curvature = calculateCurvature(curve, idx);
+    const length = 0.5 + (curvature === 0 ? 1 : Math.abs(curvature));
+    const p2 = new Point(p1.x + normal.x * length, p1.y + normal.y * length); // scale normal by curvature to visualize sharp bends
 
     interPoints.push(p1);
     interPoints.push(p2);
