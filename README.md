0fdb080c7cf88ac8533b12875fb7a506f43c6d9d
smycynek <sv@stevenvictor.net>
2026-07-14 19:49:31 -0400

    Update readme, normal density

--- README.md
+++ README.md
@@ -6,25 +6,31 @@ version: 000233
 
 # A simple spline app
 
+In my spare time, I've tried to learn more about splines.  I'm currently working on building some from scratch, including the polynomial evaluation, derivative calculation, and linear algebra solves.  I'm not there yet -- I used a nurbs library in this project, but I did the graphics, some screen to cartesian transforms, and some normal and curvature calculations myself, so that was fun.
+
+I'll combine some of this with the polynomial and
+parameterization and work I did in https://stevenvictor.net/curvebox/ and hopefully have an end-to-end spline implementation soon.
+
 ## Installation
 
+
+```bash
 // Set up and debug
 
-`bun install`
+bun install
+bun run dev
 
-`bun run dev`
 
 // Code styling
 
-`bun run lint`
-
-`bun run format`
+bun run lint
+bun run format
 
 // Deployment
 
-`bun run build`
-
-`deploy.sh`
+bun run build
+deploy.sh
+```
 
 ## Other notes
 
--- src/nurbs.ts
+++ src/nurbs.ts
@@ -16,24 +16,6 @@ export function createSplineNurbs(points: Point[], degree: number) {
   return interPoints;
 }
 
-export function createSplineNurbTangents(points: Point[], degree: number) {
-  const sPoints = points.map((p) => [p.x, p.y]);
-  const curve = nurbs(sPoints, degree);
-  const derivative = curve.evaluator(1);
-  const interPoints: Point[] = [];
-  const domain = curve.domain[0];
-  for (let idx = domain[0]; idx < domain[1]; idx += 0.01) {
-    const p1a = curve.evaluate([], idx);
-    const p1 = new Point(p1a[0], p1a[1]);
-    const dval = derivative([], idx);
-    // The derivative is a direction vector, not a point on the curve.
-    const tangent = new Vector(dval[0], dval[1], 0).normalize();
-    const p2 = new Point(p1.x + tangent.x, p1.y + tangent.y);
-    interPoints.push(p1);
-    interPoints.push(p2);
-  }
-  return interPoints;
-}
 
 function calculateCurvature(curve: ReturnType<typeof nurbs>, t: number): number {
   const d1val: [number, number] = curve.evaluator(1)([], t);
@@ -54,7 +36,7 @@ export function createSplineNurbNormals(points: Point[], degree: number) {
   const derivative = curve.evaluator(1);
   const interPoints: Point[] = [];
   const domain = curve.domain[0];
-  for (let idx = domain[0]; idx < domain[1]; idx += 0.1) {
+  for (let idx = domain[0]; idx < domain[1]; idx += 0.05) {
     const p1a = curve.evaluate([], idx);
     const p1 = new Point(p1a[0], p1a[1]);
     const dval = derivative([], idx);
